e95b6371b7ad42801afa952697eca0f27d041484
Antonio69999 <antoine.chabeauti@gmail.com>
2023-10-31 09:45:32 +0100

    able to add X's to any square

--- src/component/Square.tsx
+++ src/component/Square.tsx
@@ -1,10 +1,11 @@
 // import { useState } from "react";
 
-// interface SquareProps {
-//   value: string;
-// }
+interface SquareProps {
+  value: string;
+  onSquareClick: () => void;
+}
 
-function Square({ value, onSquareClick }) {
+function Square({ value, onSquareClick }: SquareProps) {
   //     const [value, setValue] = useState(null);
 
   return (
